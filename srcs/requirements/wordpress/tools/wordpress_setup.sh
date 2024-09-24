aac86443c6801e90777dd46b11da683966be456b
jschwabe <mail@jschwabe.site>
2024-09-24 19:10:57 +0200

    chore: fix wp config creation with envars
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
 # create config file
+wp config create --force \
+    --url=$WP_URL \
+    --dbname=$DB_NAME \
+    --dbuser=$DB_USER \
+    --dbpass=$DB_PASS \
+    --dbhost=mariadb:3306
 
 # install & configure wordpress
+wp core install --url=$WP_URL \
+    --title=$WP_TITLE \
+    --admin_user=$WP_ADMIN_USER \
+    --admin_password=$WP_ADMIN_PASS \
+    --admin_email=$WP_ADMIN_EMAIL \
+    --skip-email
 
 # create wp user
+wp user create $WP_USER \
+				$WP_USER_EMAIL \
+				--user_pass=$WP_USER_PASS
 
-# set wp home
-
-# set wp siteurl
+# set wp home and siteurl
+wp option update home $WP_URL
+wp option update siteurl $WP_URL
 
 # start FastCGI in foreground
 php-fpm82 -F
