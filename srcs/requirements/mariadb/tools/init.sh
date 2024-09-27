576e5515d7a05a28a5d599887947ca32ce3415f6
jschwabe <mail@jschwabe.site>
2024-09-27 17:36:13 +0200

    fix: static test site broken on 502
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,10 +4,11 @@
 if [ ! -d "/var/lib/mysql/${DB_NAME}" ]; then
     # pass configuration to the database daemon
 	mariadbd --user=mysql --bootstrap <<EOF
-FLUSH PRIVILEGES;
-CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
-GRANT ALL PRIVILEGES on \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+    FLUSH PRIVILEGES;
+    CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+    CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+    GRANT ALL PRIVILEGES on \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+    FLUSH PRIVILEGES;
 EOF
 fi
 
