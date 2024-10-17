7931b586d86bd967c9cc98d8c06b4e5ff788d205
jschwabe <mail@jschwabe.site>
2024-10-17 18:55:39 +0200

    chore(mariadb_exporter): use .cnf instead of script
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- srcs/requirements/logs/mariadb-exporter/dsn_init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export MYSQLD_EXPORTER_PASSWORD=${EXPORTER_PASS}
-# Run mysqld_exporter
-exec /usr/local/bin/mysqld_exporter --mysqld.address=mariadb:3306 --mysqld.username=${EXPORTER_USER}
